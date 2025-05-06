bdfd3a570573c065931c1cc5cdd46fc02778841b
kaden1670 <61885789+kaden1670@users.noreply.github.com>
2025-05-06 09:57:16 -0500

    reorganizing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="genome_organization",
     version="0.1.0",
-    packages=find_packages() + ['polykit', 'polykit.renderers'],
+    packages=find_packages() + ['polykit', 'polykit.renderers', 'utilis'],
     install_requires=[
         "numpy",
         "scipy",
--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,6 @@ Genome organization package.
 
 from .polychrom.polychrom import *
 from .polykit.polykit import *
+from .utilis import *
 
-__all__ = ['polychrom', 'polykit'] 
+__all__ = ['polychrom', 'polykit', 'utilis'] 
